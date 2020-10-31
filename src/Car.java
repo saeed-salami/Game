c1a8df92290133c1273b8112708bf08078507578
saeed-salami <saeed.salami84@gmail.com>
2020-10-31 23:01:43 +0330

    add car

---
+++ src/Car.java
@@ -0,0 +1,41 @@
+public class Car {
+    public int speed;
+    public int gear;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getGear() {
+        return gear;
+    }
+
+    public int upGear(){
+        if (gear<6 && gear>=0) {
+            speed +=40;
+            return gear += 1;
+        } else if (gear == 6){
+            return speed+=20;
+        }
+        return speed;
+    }
+    public int downGear(){
+        if (gear>0) {
+            speed -=40;
+            return gear -= 1;
+        } else if (gear == 0){
+            return speed-=20;
+        }
+        return speed;
+    }
+    public void showStatus(String str){
+        if (str.equals("up")) {
+            upGear();
+            System.out.println("fast" + gear + "\n" + speed);
+        } else if (str.equals("down")) {
+            downGear();
+            System.out.println("slow" + gear + "\n" + speed);
+        } else
+            System.err.println("Not thing");
+    }
+}
---
+++ src/Game.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) {
+        Server server = new Server();
+
+        Scanner scanner = new Scanner(System.in);
+        Car c1 = new Car();
+        while (true) {
+            String str = scanner.next();
+            if (str.equals("end")){
+                break;
+            }
+            c1.showStatus(str);
+        }
+    }
+
+}
